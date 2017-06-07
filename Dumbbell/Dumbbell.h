bc75718a85d6b7424ac3b2be32eb34a46ffb0353
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-07 12:05:04 -0700

    Initial Commit

---
+++ Dumbbell/Dumbbell.h
@@ -0,0 +1,19 @@
+//
+//  Dumbbell.h
+//  Dumbbell
+//
+//  Created by Romain Pouclet on 2017-06-07.
+//  Copyright © 2017 Buddybuild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dumbbell.
+FOUNDATION_EXPORT double DumbbellVersionNumber;
+
+//! Project version string for Dumbbell.
+FOUNDATION_EXPORT const unsigned char DumbbellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dumbbell/PublicHeader.h>
+
+
