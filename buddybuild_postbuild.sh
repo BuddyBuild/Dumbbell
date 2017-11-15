34a031163c15319d92073f637b3fc779bf95fdd8
Romain Pouclet <romain.pouclet@gmail.com>
2017-11-14 20:42:26 -0500

    WIP

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+## Make sure the script exits as soon as an error occurs
+## More info about this on this great blog post:
+## https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -e
 
+## Switch to a recent version of chruby
+##
 chruby 2.3.1
+chruby
 
 bundle install
-bundle exec scripts/release.rb
+bundle exec scripts/bump_spec_version.rb
+
